6633737cd67125553ce79e8f81d18a49d525ab2d
Alex <alex@dprk.dprk>
2019-04-11 18:13:58 +0100

    added speeds

--- calculate_mpfc_out.py
+++ calculate_mpfc_out.py
@@ -14,14 +14,27 @@ def ofc5HTreceptors(_5ht,x,t):
 
 
 if __name__ == '__main__':
+    print("Normal run values:")
 
- #  for _5ht in [0.5,1,1.5,2.0,2.5,3.0]:
-   for _5ht in [0.05,0.1,0.15,0.20,0.25,0.3]:
+    for _5ht in [0.5,1.0,1.5,2.0,2.5,3.0]:
 
       print("5ht =" + str(_5ht)+ ": " + str(round((ofc5HTreceptors(_5ht,1,1))* 100, 0)))
 
+    print("suppressed speed values")
+    for _5ht in [0.05,0.1,0.15,0.20,0.25,0.3]:
 
+             print("5ht =" + str(_5ht)+ ": " + str(round((ofc5HTreceptors(_5ht,1,1))* 100, 0)))
+
+
+    print("suppressed speed valueswith ssri")
+    for _5ht in [0.05,0.1,0.15,0.20,0.25,0.3]:
+
+             print("5ht =" + str(_5ht)+ ": " + str(round((ofc5HTreceptors(_5ht,1,2))* 100, 0)))
 
 
         
+    print("suppressed speed valueswith psi")
+    for _5ht in [0.05,0.1,0.15,0.20,0.25,0.3]:
+
+             print("5ht =" + str(_5ht)+ ": " + str(round((ofc5HTreceptors(_5ht,1,3))* 100, 0)))
 		
